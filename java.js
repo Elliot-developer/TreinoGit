9110956f572c7c53b5bd8196ce536334967dd95d
Elliot <chernoterepolho123@gmail.com>
2022-05-16 20:14:16 -0300

    Js e estrutura login

---
+++ java.js
@@ -0,0 +1,20 @@
+var c = document.getElementById ('b-cd')
+var l = document.getElementById ('b-ld')
+
+var tc = document.getElementById ('text-c')
+var tl = document.getElementById ('text-l')
+
+
+
+function cadastro(e) {
+     tc.innerHTML="Cadastrado arrombado"; 
+
+}
+
+function login() {
+     alert("Logado!!!!!!!!!!!!!")
+     console.log ("Logado:" + Date.now() + "");
+     console.log ("Usuario: ?");
+     console.log ("Senha: *******");
+     console.log ("-----------")
+}
